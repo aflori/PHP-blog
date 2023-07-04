bc7b73cc15d13e84dd9faba1e7cb46451256cdec
aflori <aurelien.flori@le-campus-numerique.fr>
2023-07-04 13:58:29 +0200

    PHP Reroad for comment creation form

---
+++ app/controllers/blogPostCreatC.php
@@ -0,0 +1,7 @@
+<?php
+include_once "app/persistences/blogPostData.php";
+
+
+include "ressources/views/layouts/header.tpl.php";
+include "ressources/views/blogCreateCommment.tpl.php";
+include "ressources/views/layouts/footer.tpl.php";
