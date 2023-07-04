7033b6c528b9709556ffe9c0aeced237d5c5859b
aflori <aurelien.flori@le-campus-numerique.fr>
2023-07-04 11:55:42 +0200

    Added comment display in html code on blogPost.tpl.php.

--- ressources/views/blogPost.tpl.php
+++ ressources/views/blogPost.tpl.php
@@ -24,4 +24,29 @@ needs $articleContent difined to print an article content with key:
         <button type="submit" name="submit" value="true">Suprimer l'article</button>
     </form>
 </section>
+<h2>
+    Les commentaires
+</h2>
+<?php if(count($lesComments) ===0): ?>
+<h4> Pas de commentaires écrits </h4>
+<?php else: ?>
+<?php foreach($lesComments as $commentaireData) : ?>
+<section style="border: solid 1px purple; margin-bottom:2px">
+    <p>
+        <?=$commentaireData['comentaire']?>
+    </p>
+    <p>
+        écrit par <i><?=$commentaireData['pseudo']?></i>
+        le <i><?=$commentaireData['dateDePublication'] ?> </i>
+    </p>
+</section>
+<?php endforeach?>
+<?php endif ?>
+
+<div>
+    <a href="http://blog.local?action=blogPostCreateComment&id=<?=$idArticle?>">
+        <button>Créer un article</button>
+    </a>
+</div>
+
 <a href="?action=index">retour à l'acueil</a>
