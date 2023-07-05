c0f8ee3795b226fafbbb7e2c043e506dfad56726
aflori <aurelien.flori@le-campus-numerique.fr>
2023-07-05 10:51:18 +0200

    html Structure of new form

---
+++ ressources/views/blogCreateComment.tpl.php
@@ -0,0 +1,35 @@
+<?php
+/*
+ * needs defined:
+ * $autorList
+ * $_SESSION['commentAuthorM']
+ * $_SESSION['commentTextM']
+ */
+?>
+<form action="?action=blogPostCreateComment" method="post">
+
+    <label>
+        <?= $_SESSION['commentTextM'] ?>
+        <textarea name="commentContent" cols="20" rows ="5"></textarea>
+    </label>
+
+    <label for="radioAutor">
+        <?= $_SESSION['commentAuthorM'] ?>
+    </label>
+    <?php foreach($autorList as $autorID): ?>
+        <div id="radioAutor">
+            <label>
+                <input
+                        type="radio"
+                        name="autorPseudo"
+                        value="<?=$autorID['id']?>"
+                    <?php
+                    if ($formSent and $autorID['id']===$_SESSION['create']['autorPseudo'])
+                    { echo "checked"; }
+                    ?> >
+                <?= $autorID["pseudoname"] ?>
+            </label>
+        </div>
+    <?php endforeach; ?>
+    <button type="submit"> Créer le commentaire </button>
+</form>
