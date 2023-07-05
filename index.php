12f529210d47c8bba3cf03e46b63643a31ce9799
aflori <aurelien.flori@le-campus-numerique.fr>
2023-07-05 10:50:56 +0200

    fix path (forget a letter)

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         'blogPostCreate' => 'blogPostCreateController.php',
         'blogPostEdit' => 'blogPostEditController.php',
         'blogPostDelet' => 'blogPostDelete.php',
-        'logPostCreateComment' => 'blogPostCreatC.php'
+        'blogPostCreateComment' => 'blogPostCreatC.php'
     ];
 
     $nomDeLaRoute = filter_input(INPUT_GET, "action",FILTER_SANITIZE_URL);
